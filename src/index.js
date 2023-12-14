027bee304ae9f875733cb61b13831d178ddf1f5c
Aika Weidman <119600413+aweidman110@users.noreply.github.com>
2023-12-14 14:12:57 -0500

    Added the forecast HTML and CSS

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ function refreshWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
-   let date = new Date(response.data.time * 1000);
+  let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
   //console.log(response.data.condition.description);
   //console.log(response.data);
-  
+
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
